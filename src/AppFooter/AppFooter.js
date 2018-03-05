e36087ca7e8aea17489db916cfb734d4b2a0dfeb
Radek Palisa <rpalisa@ebay.com>
2018-03-06 00:09:31 +0100

    href correction

--- src/AppFooter/AppFooter.js
+++ src/AppFooter/AppFooter.js
@@ -5,7 +5,7 @@ export default class AppFooter extends React.Component {
     render() {
         return (
             <footer className="App-footer">
-                <p>Built by <a href="htttp://www.radekpalisa.com">Radek Palisa</a></p>
+                <p>Built by <a href="http://www.radekpalisa.com">Radek Palisa</a></p>
             </footer>
         )
     }
